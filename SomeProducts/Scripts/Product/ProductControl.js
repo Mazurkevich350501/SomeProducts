3d7d41ef049a046263fb50d9ad1fbf85fe64ac20
Mazurkevich350501 <kiramzit13@gmail.com>
2016-08-22 16:40:10 +0300

    Added illegal character validation.

--- SomeProducts/Scripts/Product/ProductControl.js
+++ SomeProducts/Scripts/Product/ProductControl.js
@@ -1,164 +1,31 @@
-﻿var brandsList;
-var brandChangesModel = {
-    RemovedBrands: [],
-    AddedBrands: []
-};
-var idCounter;
+﻿var validationExp = /[\!@_%<>\$\^\[\]\+\-\/\{\}]/;
+$("form").submit(function(){
+	var result = true;
+	$('span').empty();
+	$('input').each(function() {
+		if(checkInputData(this)){
+			showValidationMessage(this);
+			result = false;
+		}	
+	});
+	$('textarea').each(function() {
+		if(checkInputData(this)){
+			showValidationMessage(this);
+			result = false;
+		}	
+	});
+	console.log(result);
+	return result;
+});
 
-$("#delete-btn").submit(function(){return false;})
-
-function startInit(){
-    createBrandsDiv();
-    brandChangesModel.RemovedBrands = [];
-    brandChangesModel.AddedBrands = [];
-    brandsList =[];
-    idCounter = 1;
+function checkInputData(obj){
+	return validationExp.test( $(obj).val());
 }
 
-function createBrandsDiv(){
-    $('#newBrandValidation').empty();
-    $('#modal-content').remove();       
-    $('#modal-body').append("<div id='modal-content'/>");
+function showValidationMessage(obj){
+	console.log($('#' + obj.id + "_Validation"));
+	$('#' + obj.id + "_Validation").append("Used illegal characters");
 }
 
-$('document').ready(function () {
-    $('#brandEditBtn').click(function () {
-        $.getJSON(getBrandsListUrl, function (brands) {
-            startInit();
-            brands.forEach(function (item, i, brands) {
-                addBrandTolist(item.BrandId, item.BrandName, brandsList);
-                showBrand(item.BrandId, item.BrandName, "dbBrand");
-            })
 
-        })
-
-    }) 
-
-})
-
-function removeBrand(obj) {
-    var id = obj.getAttribute('id').substring(4);
-    var name = obj.getAttribute('data-name');
-    if (obj.getAttribute('data-info') == "newBrand") {
-        removeBrandFromList(name, brandChangesModel.AddedBrands);
-    }
-    else {
-        var isRemove;
-        $.getJSON(isBrandUsingUrl + '/' + id, function (isUsing) {
-            if (!isUsing) {
-                addBrandTolist(id, $('#lbl-' + id).text(), brandChangesModel.RemovedBrands);
-                isRemove = true;
-            }
-            else{
-                isRemove = false;
-            }
-        });
-        if(!isRemove) return;
-    }
-    removeBrandFromList(name, brandsList);  
-    $('#row-' + id).remove();
-}
-
-function addNewBrand() {
-    var brandName = document.getElementById("newBrandName").value;
-    if(checkForValidity(brandName)){
-        addBrandTolist(0, brandName,  brandChangesModel.AddedBrands);
-        addBrandTolist(0, brandName, brandsList);
-        showBrand('0' + idCounter, document.getElementById("newBrandName").value, "newBrand");
-        document.getElementById("newBrandName").value = '';
-        idCounter++;    
-    }
-    else{
-        showErrorMessage(getErrorMessage(brandName));
-    }
-}
-
-function showBrand(brandId, brandName, info){
-    var rowId = "#row-" + brandId;
-    $('#modal-content').append("<div class='row' id='row-" + brandId + "'/>");
-    $(rowId).append("<h4 class='col-lg-10' id='lbl-" + brandId + "'>" + brandName + "</h4>");
-    $(rowId).append("<button class='btn btn-warning glyphicon glyphicon-minus-sign' onclick='removeBrand(this)' data-info='"
-        + info +"' data-name='"+ brandName +"' id='btn-" + brandId + "'/>");
-}
-
-function addBrandTolist(id, name, list) {
-    var brand = {};
-    brand.brandId = id;
-    brand.brandName = name;
-    list.push(brand);
-}
-
-function removeBrandFromList(name, list){
-    list.find(function (element, index, array) {
-        if (element.brandName == name) {
-            array.splice(index, 1);
-            return true;
-        }
-        else {
-            return false;
-        }
-    })
-}
-
-function saveBrandsChanges() {
-    postJSONData(JSON.stringify(brandChangesModel), saveBrandsChangesUrl);
-}
-
-function postJSONData(JSONData, url) { 
-    $.ajax({
-        type: 'POST',
-        url: url,
-        contentType: "application/json; charset=utf-8",
-        dataType: "json",
-        data: JSONData,
-        success: function (data) {
-            setBrandDropdownItems(data);        
-            closeModalWindow();
-        }   
-    });
-}
-
-function setBrandDropdownItems(items){
-    var id = "#" + brandDropdownId;
-    $(id).find('option').remove();
-    items.forEach(function(item, index, array){
-        addOptionToSelectBox(id, item.BrandId, item.BrandName);
-    })
-}
-
-function addOptionToSelectBox(idSelectBox, key, value){
-    $(idSelectBox).append('<option value="' + key + '">' + value + '</option>');
-}
-
-function closeModalWindow(){
-    $('#BrandModal').modal("hide");
-}
-
-function checkForValidity(brandName){
-    $('#newBrandValidation').empty();
-    if(getErrorMessage(brandName) == ""){
-        return true;
-    }
-    else{
-        return false;
-    }
-}
-
-function getErrorMessage(brandName){
-    var message = "";
-    if(brandName == "") {
-        message = "Brand name is empty.";
-    }
-    brandsList.forEach(function(brand, index, array){
-        if(brand.brandName == brandName){
-            message = "Brand name already exists.";
-        } 
-    });
-    console.log(message);
-    return message;
-}
-
-function showErrorMessage(message){
-    console.log(message);
-    $('#newBrandValidation').append(message);
-}
+	
