0225f10ed607b4505c9f19b93d0130be5d0aa913
Mazurkevich350501 <kiramzit13@gmail.com>
2016-08-23 17:35:22 +0300

    Added database project.

---
+++ SomeProductsDatabase/dbo/Tables/Brands.sql
@@ -0,0 +1,6 @@
+﻿CREATE TABLE [dbo].[Brands] (
+    [BrandId]   INT            IDENTITY (1, 1) NOT NULL,
+    [BrandName] NVARCHAR (200) NOT NULL,
+    CONSTRAINT [PK_table1_BrandId] PRIMARY KEY CLUSTERED ([BrandId] ASC)
+);
+
---
+++ SomeProductsDatabase/dbo/Tables/Products.sql
@@ -0,0 +1,13 @@
+﻿CREATE TABLE [dbo].[Products] (
+    [ProductId]   INT             IDENTITY (1, 1) NOT NULL,
+    [Name]        NVARCHAR (MAX)  NOT NULL,
+    [Description] NVARCHAR (MAX)  NULL,
+    [Color]       NVARCHAR (50)   NOT NULL,
+    [Quantity]    INT             NOT NULL,
+    [Image]       VARBINARY (MAX) NULL,
+    [ImageType]   NVARCHAR (MAX)  NULL,
+    [BrandId]     INT             NOT NULL,
+    CONSTRAINT [PK_Products_ProductId] PRIMARY KEY CLUSTERED ([ProductId] ASC),
+    FOREIGN KEY ([BrandId]) REFERENCES [dbo].[Brands] ([BrandId])
+);
+
