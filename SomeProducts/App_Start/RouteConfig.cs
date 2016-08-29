cf1968b3b7a24ad191a569e4109b6748d5bdcc79
Mazurkevich350501 <kiramzit13@gmail.com>
2016-08-29 14:36:49 +0300

    Deleted useless files.

--- SomeProducts/App_Start/RouteConfig.cs
+++ SomeProducts/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace SomeProducts
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Product", action = "Create", id = UrlParameter.Optional }
             );
         }
     }
--- SomeProducts/Startup.cs
+++ SomeProducts/Startup.cs
@@ -8,7 +8,7 @@ namespace SomeProducts
     {
         public void Configuration(IAppBuilder app)
         {
-            ConfigureAuth(app);
+
         }
     }
 }
