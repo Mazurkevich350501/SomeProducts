7ac5853d43cbe10c9fd3f9f0ead20fda02c4dd56
Mazurkevich350501 <kiramzit13@gmail.com>
2016-09-01 11:53:22 +0300

    Removed brand and product validation.

--- SomeProducts.DAL/Models/Brand.cs
+++ SomeProducts.DAL/Models/Brand.cs
@@ -1,13 +1,10 @@
-﻿using System.ComponentModel.DataAnnotations;
-
+﻿
 namespace SomeProducts.DAL.Models
 {
     public class Brand
     {
         public int BrandId { get; set; }
-        [Required]
-        [MaxLength(200, ErrorMessage = "Name cannot be longer than 200 characters.")]
-        [Display(Name = "Product name")]
+
         public string BrandName { get; set; }
     }
 }
--- SomeProducts.PresentationServices/Models/BrandModel.cs
+++ SomeProducts.PresentationServices/Models/BrandModel.cs
@@ -1,15 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.ComponentModel.DataAnnotations;
 
 namespace SomeProducts.PresentationServices.Models
 {
     public class BrandModel
     {
         public int BrandId;
-
+        [Required]
+        [MaxLength(200, ErrorMessage = "Name cannot be longer than 200 characters.")]
+        [Display(Name = "Product name")]
         public string BrandName;
     }
 }
