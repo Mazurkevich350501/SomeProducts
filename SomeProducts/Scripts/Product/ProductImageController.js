e1d992def85137003c6856ff8cda28106c80d7e7
Mazurkevich350501 <kiramzit13@gmail.com>
2016-09-01 18:28:40 +0300

    Deleted global variables.Fixed colorpicker bug.

--- SomeProducts/Scripts/Product/ProductImageController.js
+++ SomeProducts/Scripts/Product/ProductImageController.js
@@ -1,27 +1,34 @@
-﻿function onFileSelect(e) {
-    var file = e.target.files[0];
-    var reader = new FileReader;
-    var place = $("#image");
-    reader.readAsDataURL(file);
+﻿(function() {
+    "use strict";
+    
+    var imageNamespace = Utils.getNamespace("Image");
 
-    reader.onload = function (e) { // Как только картинка загрузится
-        place.attr("src", e.target.result);
+    imageNamespace.initImage = function(params){
+        var image = {
+            data : $("#" + params.id.imageDataId).val(),
+            type : $("#" + params.id.imageTypeId).val()
+        };
+        if(image.data !== "" && image.type !== ""){
+            $("#image").attr("src", "data:" + image.type + ";base64," + image.data);
+        }
+        else{
+            $("#image").attr("src", "https://i.vimeocdn.com/portrait/1274237_300x300");
+        }
     }
-};
 
-if (window.File && window.FileReader && window.FileList && window.Blob) {
-    document.querySelector("input[type=file]").addEventListener("change", onFileSelect, false);
-};
+    function onFileSelect(e) {
+        var file = e.target.files[0];
+        var reader = new FileReader;
+        var place = $("#image");
+        reader.readAsDataURL(file);
 
-$("document").ready(function (){
-	var image = {
-		data : $("#" + imageDataId).val(),
-		type : $("#" + imageTypeId).val()
-	};
-	if(image.data !== "" && image.type !== ""){
-		$("#image").attr("src", "data:" + image.type + ";base64," + image.data);
-	}
-    else{
-        $("#image").attr("src", "https://i.vimeocdn.com/portrait/1274237_300x300");
-    }
-})
+        reader.onload = function (e) {
+            place.attr("src", e.target.result);
+        }
+    };
+
+    if (window.File && window.FileReader && window.FileList && window.Blob) {
+        document.querySelector("input[type=file]").addEventListener("change", onFileSelect, false);
+    };
+   
+})();
