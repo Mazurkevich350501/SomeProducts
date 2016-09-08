30076c49af3dca4fa532d438e67dc79566a692b3
Mazurkevich350501 <kiramzit13@gmail.com>
2016-09-08 18:02:16 +0300

    Change autofac container builder.

--- SomeProducts.PresentationServices/PresentaoinServices/BrandModelPresentationService.cs
+++ SomeProducts.PresentationServices/PresentaoinServices/BrandModelPresentationService.cs
@@ -1,6 +1,5 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
-using SomeProducts.DAL.Dao;
 using SomeProducts.DAL.IDao;
 using SomeProducts.DAL.Models;
 using SomeProducts.PresentationServices.IPresentationSevices;
--- SomeProducts/App_Start/ContainerConfig.cs
+++ SomeProducts/App_Start/ContainerConfig.cs
@@ -14,13 +14,13 @@ namespace SomeProducts
         public static IContainer CreateContainer()
         {
             var builder = new ContainerBuilder();
-            builder.RegisterType<BaseRepository<Product>>().As<IRepository<Product>>().SingleInstance();
-            builder.RegisterType<BaseRepository<Brand>>().As<IRepository<Brand>>().SingleInstance();
-            builder.RegisterType<ProductDao>().As<IProductDao>().SingleInstance();
-            builder.RegisterType<BrandDao>().As<IBrandDao>().SingleInstance();
-            builder.RegisterType<ProductViewModelPresentationService>().As<IProductViewModelPresentationService>().SingleInstance();
-            builder.RegisterType<BrandModelPresentationService>().As<IBrandModelPresentationService>().SingleInstance();
-            builder.RegisterType<ProductController>().As<ProductController>();
+            builder.RegisterType<BaseRepository<Product>>().As<IRepository<Product>>().InstancePerRequest();
+            builder.RegisterType<BaseRepository<Brand>>().As<IRepository<Brand>>().InstancePerRequest();
+            builder.RegisterType<ProductDao>().As<IProductDao>().InstancePerRequest();
+            builder.RegisterType<BrandDao>().As<IBrandDao>().InstancePerRequest();
+            builder.RegisterType<ProductViewModelPresentationService>().As<IProductViewModelPresentationService>().InstancePerRequest();
+            builder.RegisterType<BrandModelPresentationService>().As<IBrandModelPresentationService>().InstancePerRequest();
+            builder.RegisterType<ProductController>().As<ProductController>().InstancePerRequest();
             return builder.Build();
         }
     }
