d17dc4756d7cddaa1f7e22a0c5d6c9656618225d
Mazurkevich350501 <kiramzit13@gmail.com>
2016-09-09 18:27:51 +0300

    Added unit tests for BrandModelPresentationService.

--- SomeProducts.PresentationServices.Test/BrandModelPresentationServiceTest.cs
+++ SomeProducts.PresentationServices.Test/BrandModelPresentationServiceTest.cs
@@ -13,6 +13,22 @@ namespace SomeProducts.PresentationServices.Test
     [TestClass]
     public class BrandModelPresentationServiceTest
     {
-        
+        [TestMethod]
+        public void GetAllItems_Should_Return_All_Brands()
+        {
+            var brandDao = new Mock<IBrandDao>();
+            var service = new BrandModelPresentationService(brandDao.Object);
+            var brands = new List<Brand>
+            {
+                new Brand() {BrandId = 1, BrandName = "Name1"},
+                new Brand() {BrandId = 2, BrandName = "Name2"}
+            };
+            brandDao.Setup(d => d.GetAllItems()).Returns(brands);
+
+            var result = service.GetAllItems();
+
+            Assert.IsNotNull(result);
+            Assert.AreEqual(brands.Count, result.ToList().Count);
+        }
     }
 }
